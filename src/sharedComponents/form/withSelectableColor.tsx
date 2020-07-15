34b6b2aea86c0e8135e157a2ffe4276f130d317d
Linda Paiste <lindapaiste@gmail.com>
2020-07-15 16:38:13 -0500

    throttle

--- src/sharedComponents/form/withSelectableColor.tsx
+++ src/sharedComponents/form/withSelectableColor.tsx
@@ -1,6 +1,7 @@
 import React, {ComponentType, FunctionComponent, useState} from "react";
 import {I_ColorAdapter, randomColor} from "../../packages/color-adapter";
 import {SelectColor} from "./SelectColor";
+import debounce from "lodash/debounce";
 
 /**
  * for tools which are initialized with a random color,
@@ -9,18 +10,23 @@ import {SelectColor} from "./SelectColor";
 
 
 /**
- * might need to throttle it being passed down as it can change rapidly when dragging
+ * need to debounce/throttle the color being being passed down
+ * because it can change rapidly when dragging
+ * and wrapped component may have expensive operations dependent on color
  */
 
 export const withSelectableColor = <P extends {color: I_ColorAdapter}>(Component: ComponentType<P>): FunctionComponent<Omit<P, 'color'>> =>
     (props) => {
         const [color, setColor] = useState(randomColor());
+        //const [cachedColor, setCachedColor] = useState(randomColor());
+
+        const onChange = debounce(setColor, 250, { trailing: true });
 
         return (
             <div>
                 <div>
                     <div>Select Color:</div>
-                    <SelectColor color={color} onChange={setColor}/>
+                    <SelectColor color={color} onChange={onChange}/>
                 </div>
                 <Component
                     {...{...props, color} as P}
